62a8c62e1afd3d686511ed8ed854e9721ca5e87d
Arthur Valentim Kasper <arthur.kasper@rdstation.com>
2021-06-15 19:29:05 -0300

    First components

---
+++ src/container/Game/GamePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Board, Card } from '../../components'; 
+
+const GamePage = () => (
+  <Board>
+    <Card name="Testing Card"/>
+  </Board>
+);
+
+export default GamePage;
